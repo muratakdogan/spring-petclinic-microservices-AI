b399c8a8cd3a3ecf03289f81d1203e9f0f706b25
muratakdogan <muratakdogan86@gmail.com>
2025-08-08 11:30:38 +0000

    jenkins-ecr-registry-create.sh oluşturuldu

---
+++ .devops/jenkins/jenkins-ecr-registry-create.sh
@@ -0,0 +1,10 @@
+
+APP_REPO_NAME="cloudandcloud-repo/capstone-microservice-app-dev"
+AWS_REGION="us-east-1"
+
+aws ecr describe-repositories --region ${AWS_REGION} --repository-name ${APP_REPO_NAME} || \
+aws ecr create-repository \
+--repository-name ${APP_REPO_NAME} \
+--image-scanning-configuration scanOnPush=false \
+--image-tag-mutability MUTABLE \
+--region ${AWS_REGION}
